971783f49077061a623086bd948821bd148ac42f
NingTianhao5 <NingTianhao5@huaqin.com>
2020-05-02 15:05:32 +0800

    FireWorks1

---
+++ Android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/zx/Desktop/Adreno_GPU_SDK/Development/Samples/OpenGLES30/FireWorks1/Android/bin/AndroidManifest.xml : \
+/home/zx/Desktop/Adreno_GPU_SDK/Development/Samples/OpenGLES30/FireWorks1/Android/AndroidManifest.xml \
---
+++ Android/bin/NativeActivity-debug-unaligned.apk.d
@@ -0,0 +1,3 @@
+/home/zx/Desktop/Adreno_GPU_SDK/Development/Samples/OpenGLES30/FireWorks1/Android/bin/NativeActivity-debug-unaligned.apk : \
+/home/zx/Desktop/Adreno_GPU_SDK/Development/Samples/OpenGLES30/FireWorks1/Android/bin/NativeActivity.ap_ \
+/home/zx/Desktop/Adreno_GPU_SDK/Development/Samples/OpenGLES30/FireWorks1/Android/libs/armeabi-v7a/libFireWork1.so \
